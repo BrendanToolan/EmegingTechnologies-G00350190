8998d10b8051066270af318a44248b5e9281b718
Brendan Toolan <btoolan73@gmail.com>
2019-12-03 18:25:28 +0000

    static folder added and html page created. Error regarding line 6 in the py file

--- WebApp/webApp.py
+++ WebApp/webApp.py
@@ -2,8 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello'
+def home():
+    return render_template('WebApp/static/homepage.html')
+
 
 if __name__ == "__main__":
     app.run
