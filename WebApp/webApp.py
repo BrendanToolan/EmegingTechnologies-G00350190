fa06c2dcedd011d376a0cf5f0ed8bd0a6820d5b6
Brendan Toolan <btoolan73@gmail.com>
2019-12-09 22:14:14 +0000

    Code that predicts handwritten digit commented on py file. Errors in the code

--- WebApp/webApp.py
+++ WebApp/webApp.py
@@ -29,3 +29,25 @@ def uploadimage():
         f.write(decodedimg)
     
     return {"message": theimage}
+
+#@app.route('/predict', methods=['GET','POST'])
+#def predict():
+#    parseImg(request.get.data())
+
+#    x = imread("theimage.png", mode='L')
+ #   x = np.invert(x)
+  #  x = imresize(x, (28,28))
+
+#    x = x.reshape(1, 28, 28, 1)
+#    with graph.as_default():
+#        out = models.predict(x)
+#        print(out)
+#        print(np.argmax(out, axis=1))
+#        response = np.array_str(np.argmax(out, axis=1))
+#        return response
+#        return {"message": theimage}
+#
+#    def parseImg(imgData):
+#        imgstr = re.search(b'base64,(.*)', imgData).group(1)
+#        with open('theimage.png', 'wb') as output:
+#            output.write(base64.decodebytes(imgstr))
