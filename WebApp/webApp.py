5bcdf981c70688263af4e82e10587a8f214fb96f
Brendan Toolan <btoolan73@gmail.com>
2019-12-09 18:37:30 +0000

    Model is connected to the flask

--- WebApp/webApp.py
+++ WebApp/webApp.py
@@ -1,11 +1,15 @@
+# Adapted from https://www.palletsprojects.com/p/flask/
+# Adapted from https://medium.com/analytics-vidhya/deploy-ml-models-using-flask-as-rest-api-and-access-via-flutter-app-7ce63d5c1f3b
+
 from flask import Flask, render_template, request
 import base64
 import numpy as np
-import keras as kr
+from tensorflow.keras.models import load_model
 import flask as fl
 
-
 app = Flask(__name__)
+models = load_model('model.h5')
+
 
 @app.route('/')
 def home():
@@ -15,7 +19,7 @@ def home():
 if __name__ == "__main__":
     app.run
 
-
+#Code used to save the canvas a png image
 @app.route('/uploadimage', methods =['GET', 'POST'])
 def uploadimage():
     theimage = (fl.request.values.get("theimage",""))
